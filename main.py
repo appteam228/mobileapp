c981538776093790e3ab6090562bfdccf7c8dcfc
appteam228 <ghfdfjdkjfkdj@gmail.com>
2023-04-17 23:12:56 +0300

    all changes were added

--- main.py
+++ main.py
@@ -4,31 +4,33 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.core.window import Window
+from kivy.uix.textinput import TextInput
 
 
 
 
-class BoxLayoutExample(BoxLayout):
+class FirstLayout(BoxLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.orientation = "horizontal"
-        """btn_1 = Button(text="just a button", color=(0, 0, 255))
-        btn_2 = Button(text="just a button", color=(255, 0, 0))
-        self.add_widget(btn_1)
-        self.add_widget(btn_2)"""
-    
-    def hello_world(self):
-        print("hello world")
+        #btn_1 = Button(text="just a button", color=(0, 0, 255))
+        #btn_2 = Button(text="just a button", color=(255, 0, 0))
+        #self.add_widget(btn_1)
+        #self.add_widget(btn_2)
 
-
+    def test_foo(self):
+        #text = self.ids.text_in.text
+        #print(text)
+        pass
 
 class MainWidget(Widget):
     pass
 
-
 class TheLabelApp(App):
     def build(self):
-        return BoxLayoutExample()
+        #Window.clearcolor = (1, 1, 1, 1)
+        return FirstLayout()
 
 if __name__ == "__main__":
     app = TheLabelApp().run()
